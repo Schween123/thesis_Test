3c79dff4da3635320b78da600d9901c9136bc5b4
Schween123 <130567321+Schween123@users.noreply.github.com>
2024-08-01 09:22:12 +0800

    abc

--- frontend/src/pages/admin/BHRegistration/BHRegistration.tsx
+++ frontend/src/pages/admin/BHRegistration/BHRegistration.tsx
@@ -19,7 +19,7 @@ export default function BHRegistration() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/boardinghouses/', {
+      const response = await fetch('http://192.168.8.102:8000/api/boardinghouses/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
